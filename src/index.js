37a47efbcd8ff7c7d211c320749e23b3765bf1f6
eric martinez <eamartinez.acelas@outlook.com>
2020-06-21 22:43:26 -0500

    reparado index en rutas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const indexRoute = require('./routes');
+const indexRoute = require('./Routes');
 
 //settings
 app.set('port', 8000);
