d5fd4bed26d1704bbd4d297c4d22bf2df5be5506
eric martinez <eamartinez.acelas@outlook.com>
2020-06-21 20:30:57 -0500

    agregado proyecto

---
+++ src/Routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const titleIndex = "FindBook";
+
+
+router.get('/', (req, res) => {
+    res.render('index.html', { titleIndex });
+});
+router.get('/categorias', (req, res) => {
+    res.render('categorias.html', { titleIndex });
+});
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { titleIndex });
+});
+router.get('/login', (req, res) => {
+    res.render('login.html', { titleIndex });
+});
+router.get('/support', (req, res) => {
+    res.render('support.html', { titleIndex });
+});
+router.get('/term_cond', (req, res) => {
+    res.render('term_cond.html', { titleIndex });
+});
+
+
+module.exports = router;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+//assing
+const express = require('express');
+const app = express();
+const path = require('path');
+const indexRoute = require('./routes');
+
+//settings
+app.set('port', 8000);
+app.set('views', path.join(__dirname, 'Views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+
+//routes
+app.use(indexRoute);
+
+//static files
+app.use(express.static(path.join(__dirname, 'Public')));
+
+//listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port:', app.get('port'));
+});
