3c6bbf4d1fcf61faffc4c94e3af90f50074a2459
eric martinez <eamartinez.acelas@outlook.com>
2020-06-28 12:54:56 -0500

    pruebas iniciando pruebas nodejs

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../src/index.js');
-
-describe('GET /', function() {
-	if( function(done) {
-		request(app).get('/').expect('index run', done);
-	}); 
-});
