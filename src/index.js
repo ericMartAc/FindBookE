1e177fa2b559d3e39e30cdbc68567e5741935703
Eric Martinez Acelas <42032838+ericMartAc@users.noreply.github.com>
2020-06-28 10:51:35 -0500

    Update index.js
    retornando puerto

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ const app = require('./config/server');
 //routes
 require('./app/Routes')(app);
 
-//listen the server
-app.listen(app.get('port'));
+//listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port: ', app.get('port'));
+});
